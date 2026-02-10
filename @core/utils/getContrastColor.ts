2c02dc948a106a88ade483fba8986168b7644c54
Samia Asmar <asmarsamia2003@gmail.com>
2026-02-10 15:55:04 +0200

    Solve comments

--- @core/utils/getContrastColor.ts
+++
@@ -1,8 +0,0 @@
-import { getLuminance } from './getLuminance'
-
-export function getContrastColor(backgroundColor: string, lightColor = 'FFFFFF', darkColor?: string): string {
-  const luminance = getLuminance(backgroundColor)
-  const darkTextColor = darkColor || '2D2F31'
-
-  return luminance > 0.5 ? darkTextColor : lightColor
-}
